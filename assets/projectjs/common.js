01aeadb37b51ad1a370ba5de6b0b0433fd17e2e1
kwahome <kevowahome@gmail.com>
2015-07-20 13:05:45 +0300

    Select facility function

--- assets/projectjs/common.js
+++ assets/projectjs/common.js
@@ -1,10 +1,20 @@
 /*Function to change the icon in the collapsible menu*/
-function changeIcon()
+function changeIcon(itemID)
 {
-    var icon=$(".changed");
+    var icon=$("#"+itemID);
+
     if (icon.hasClass("glyphicon-plus-sign"))
     {
         icon.removeClass("glyphicon-plus-sign").addClass("glyphicon-minus-sign");
+
+        // if(icon.hasClass("fa fa-folder-o"))
+        // {
+        //     icon.removeClass("fa fa-folder-o").addClass("fa fa-folder-open");
+        // }
+        // else
+        // {
+        //     icon.removeClass("fa fa-folder-open").addClass("fa fa-folder-o");
+        // } 
     }
     else if (icon.hasClass("glyphicon-minus-sign"))
     {
@@ -13,7 +23,7 @@ function changeIcon()
     else
     {
         icon.removeClass("glyphicon-minus-sign").addClass("glyphicon-plus-sign");
-    }                
+    }            
 
 }
 /*--------------------------------------------------------------------------------------------------------------------------------*/
@@ -21,14 +31,34 @@ function changeIcon()
 /*Function to select a facility for analytics*/
 function selectFacility(facilityID)
 { 
+    var any = $(".color");
     var facility=$("#"+facilityID);
-    if (facility.hasClass("unclickedColor"))
+
+    if (any.hasClass("clickedColor selectedFacility"))
     {
-        facility.removeClass("unclickedColor").addClass("clickedColor selectedFacility");
+        any.removeClass("clickedColor selectedFacility").addClass("unclickedColor");
+
+        if (facility.hasClass("unclickedColor"))
+        {
+            facility.removeClass("unclickedColor").addClass("clickedColor selectedFacility");
+        }
+        else
+        {
+            facility.removeClass("clickedColor selectedFacility").addClass("unclickedColor");
+        }
     }
+    
     else
     {
-        facility.removeClass("clickedColor selectedFacility").addClass("unclickedColor");
+        if (facility.hasClass("unclickedColor"))
+        {
+            facility.removeClass("unclickedColor").addClass("clickedColor selectedFacility");
+        }
+        else
+        {
+            facility.removeClass("clickedColor selectedFacility").addClass("unclickedColor");
+        }
+
     }
     
 }
