b7705bb8d4685f32be35abc2c70f5b96baeebbf4
kwahome <kevowahome@gmail.com>
2015-07-22 17:31:52 +0300

    Ordering points distribution DB script

--- db/fetch/ordering_points_distribution.php
+++ db/fetch/ordering_points_distribution.php
@@ -17,7 +17,7 @@
         $program = $_GET['program_id'];
         $org_unit = $_GET['org_unit'];
         $org_unit_level = $_GET['org_unit_level'];
-        
+
         // Fetch all sub counties whose parent are the current county
         $sub_counties_query = "SELECT * FROM sub_counties WHERE parent_id = '$org_unit'";
         $answer = mysqli_query($conn,$sub_counties_query);
@@ -31,16 +31,23 @@
                 $count = $count+1;
             }
 
+
             $i=0;
+            $scs_count = 0;
+            $cs_count = 0;
+            $sa_count = 0;
+
             for($i=0;$i<$count;$i++)
             {
                 $parent_id = $sc_id[$i];
-                $sites = "SELECT * FROM facility_program_mapping WHERE classification = 'Satellite Site' 
-                OR classification = 'StandAlone' AND program_id = '$program'";
-                $sites_response = mysqli_query($conn,$sites);
-                if(mysqli_num_rows($sites_response)>0)
+
+                // Sub-County Stores
+                $sub_county_stores = "SELECT * FROM facility_program_mapping WHERE classification = 'Sub-County Store'
+                AND program_id = '$program'";
+                $sub_county_stores_response = mysqli_query($conn,$sub_county_stores);
+                if(mysqli_num_rows($sub_county_stores_response)>0)
                 {
-                    while($row = mysqli_fetch_assoc($sites_response))
+                    while($row = mysqli_fetch_assoc($sub_county_stores_response))
                     {
                         $id = $row['facility_id'];
 
@@ -49,15 +56,75 @@
                         $response= mysqli_query($conn,$facility_name);
                         if(mysqli_num_rows($response)>0)
                         {
-                            while($the_row = mysqli_fetch_assoc($response)) 
-                            {
-                                $facility_data[]= $the_row;
-                            }
+                            // Number of sub-county stores
+                            $scs_count++;
+                        }
+                    }
+                }
+                //var_dump($facility_data);
+
+
+                // Central Sites
+                $central_sites = "SELECT * FROM facility_program_mapping WHERE classification = 'Central Site'
+                AND program_id = '$program'";
+                $central_sites_response = mysqli_query($conn,$central_sites);
+                if(mysqli_num_rows($central_sites_response)>0)
+                {
+                    while($row = mysqli_fetch_assoc($central_sites_response))
+                    {
+                        $id = $row['facility_id'];
+
+                        $facility_name = "SELECT * FROM facilities WHERE facility_id = '$id' AND parent_id = 
+                        '$parent_id' ORDER BY facility_name";
+                        $response= mysqli_query($conn,$facility_name);
+                        if(mysqli_num_rows($response)>0)
+                        {
+                            // Number of sub-county stores
+                            $cs_count++;
+                        }
+                    }
+                }
+
+                // StandAlone Sites
+                $standalone_sites = "SELECT * FROM facility_program_mapping WHERE classification = 'StandAlone'
+                AND program_id = '$program'";
+                $standalone_sites_response = mysqli_query($conn,$standalone_sites);
+                if(mysqli_num_rows($standalone_sites_response)>0)
+                {
+                    while($row = mysqli_fetch_assoc($standalone_sites_response))
+                    {
+                        $id = $row['facility_id'];
+
+                        $facility_name = "SELECT * FROM facilities WHERE facility_id = '$id' AND parent_id = 
+                        '$parent_id' ORDER BY facility_name";
+                        $response= mysqli_query($conn,$facility_name);
+                        if(mysqli_num_rows($response)>0)
+                        {
+                            // Number of sub-county stores
+                            $sa_count++;
                         }
                     }
                 }
             }
+
+            // Sub-county stores
+            $facility_data[] = $scs_count;
+
+            // Central Sites
+            $facility_data[] = $cs_count;
+
+            // StandAlone sites
+            $facility_data[] = $sa_count;
+
+            // Total
+            $Total = $scs_count+$cs_count+$sa_count;
+            $facility_data[] = $Total;
+
+            // County ID
+            $facility_data[] = $org_unit;
+            
             $return = json_encode($facility_data);
             echo $return;
+        }
     }
 ?> 
